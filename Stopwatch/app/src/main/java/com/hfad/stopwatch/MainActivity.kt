d20f27214423a7a80ef8b306f9534d0df62f01f6
DESKTOP-LI4GBNA\Anastasiia <anastasiia.sh28@gmail.com>
2026-01-02 20:42:46 +0200

    Initial commit

---
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.hfad.stopwatch
+
+import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var stopwatch: Chronometer
+    var running = false
+    var offset: Long = 0
+
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        if(savedInstanceState != null){
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if(running){
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            }else setBaseTime()
+
+        }
+
+        val startButton = findViewById<Button>(R.id.start_btn)
+        val pauseButton = findViewById<Button>(R.id.pause_btn)
+        val resetButton = findViewById<Button>(R.id.reset_btn)
+
+        startButton.setOnClickListener {
+            if(!running){setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+
+        }
+
+        pauseButton.setOnClickListener {
+            if(running){
+                saveOffset()
+                stopwatch.stop()
+                running = false
+            }
+        }
+
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+
+
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(running){
+            saveOffset()
+            stopwatch.stop()
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        if(running){
+            setBaseTime()
+            stopwatch.start()
+            offset=0
+
+        }
+    }
+
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+    fun setBaseTime(){
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    fun saveOffset(){
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+
+}
